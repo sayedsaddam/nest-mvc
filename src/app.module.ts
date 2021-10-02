0a8b926df1d389a8cc011f9ab010092b8e914dee
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2021-10-02 17:13:49 +0500

    a quick fix in the mongoose connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { JobsModule } from './jobs/jobs.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [JobsModule, MongooseModule.forRoot('mongodb://localhost:27017/jobs')],
   controllers: [AppController, JobsController],
   providers: [AppService, JobsService],
 })
